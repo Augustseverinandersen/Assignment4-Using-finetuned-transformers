6e0ae34e263b818ecdfad687c2c574db705cde90
Augustseverinandersen <augustseverinandersen@hotmail.com>
2023-05-09 20:03:37 +0200

    code

--- README.md
+++ README.md
@@ -3,18 +3,20 @@
 
 # Assignment 4 - Using finetuned transformers via HuggingFace
 
-# Contribution 
+## Contribution 
 
-- The code used in this assignment is inspired from the class notebooks, and in collaboration with other students. All comments are written by me. 
+- The code used in this assignment is inspired from the in-class notebooks, and in collaboration with other students. All comments are written by me. 
 - In this assignment I have used an emotion classifier made by Jochen Hartmann. Jochen Hartmann, "Emotion English DistilRoBERTa-base". https://huggingface.co/j-hartmann/emotion-english-distilroberta-base/, 2022. The classifier has 7 catogires, anger, disgust, fear, joy, neutral, sadness, and surprise. It has been trained on 6 datasets, and is built on BERT. 
-- The data used in this assignment is from Kaggle user Jillani Soft Tech: https://www.kaggle.com/datasets/jillanisofttech/fake-or-real-news The data is a csv file containing Fake and Real news. Their are four columns:
+- The data used in this assignment is from Kaggle user [Jillani Soft Tech:](https://www.kaggle.com/datasets/jillanisofttech/fake-or-real-news) The data is a csv file containing Fake and Real news. Their are four columns:
 1. number: ID
 2. title: The title of the news article
 3. text: The text in the news article
 4. label: The label is either fake or real. 
 
-# Assignment descripition 
+## Packages 
 
+## Assignment descripition 
+Written by Ross:
 For this assignment, you should use ```HuggingFace``` to extract information from the *Fake or Real News* dataset that we've worked with previously.
 
 You should write code and documentation which addresses the following tasks:
@@ -27,28 +29,28 @@ You should write code and documentation which addresses the following tasks:
   - Distribution of emotions across *only* the fake news
 - Comparing the results, discuss if there are any key differences between the two sets of headlines
 
-# Methods / what the code does 
+## Methods / what the code does 
 
 - The code loads the data in from a user specified path. Gets the model from huggingface by using pipeline, and specifying which model to get. The data is filtered and cleaned to generate a list of all headings, all real headings, and all fake headings. Thereafter, three loops are used to take each heading in the list, and use the emotion classifer to get the label (emotion) with the highest score, and store it in an empty list. Than using python code, each emotion occorance is counted, for all headings, real headings, and fake headings. After some data rangling, splitting dictionary key and value pairs, the data is visualized into four visualizations, created with matplotlib.
 
-# Discussion
+## Discussion
 
-## All Emotions Bar
+### All Emotions Bar
 - This bar plot gives an overview of the general distripution of emotions for all headings. A vast majority of the headings are classified as neutral, followed by fear, anger, sadness, disgust, surprise, and than joy. 
-## Real Emotions Bar
+### Real Emotions Bar
 -  This bar plot shows the distripution of emotions for real news articles. Again, the vast majority are categorised as neutral, followed by the same ranking of emotions as the previous plot. 
-## Fake Emotions Bar
+### Fake Emotions Bar
 - This bar plot shows the distripution of emotions for all the fake news articles. The majority are categorised as neutral, but here disgust is slightly bigger than, sadness. 
-## All, Real, Fake Emotions Scatterplot
+### All, Real, Fake Emotions Scatterplot
 - This scatterplot shows all three categorises (all, real, fake) in one plot. This plot clearly displays how common the emotion label, is for each category. It also shows how real and fake headlines, nearly are identical for each emotion.
 
-## Discussion 
+### Findings 
 - By using the plots I can clearly state the the majority of emotions are classified as netural. This could lead to the assumption that the headlines are mostly factual, and the author not using emotion to try to sway the reader in a certain direction. Another clearly statement, which can be made, is that mostly negative emotions are used in news headlines, such as fear and anger. This portays nicely to the "real world", where most news articles are of murders or problems in society. 
 
 ### Future analysis 
 - It could be interesting to use POS, to see which kinds of words are used in the headline that give an emotion. By doing so, you could also see which emotion charged word is used the most.
 
-# Usage
+## Usage
 
 To use the script I have created follow these steps, after cloning the repository:
 
--- src/emotions.py
+++ src/emotions.py
@@ -1,52 +1,66 @@
+#Huggingface pipeline
 from transformers import pipeline 
-import pandas as pd 
-import os 
-from collections import Counter
-import matplotlib.pyplot as plt
 
-import argparse
+# Data munging tools 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import zipfile
+
+# System tools
 import sys
 sys.path.append(".")
-
-
+import argparse
+import os 
 
 
 def input_parse():
     # initialize the parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filepath", type=str) # argument is filepath as a string
+    parser.add_argument("--zip_path", type=str, help = "The path to the zip folder") # argument is zippath as a string
     args = parser.parse_args()
 
     return args
 
 
-def loading_data(args):
-    print("Loading data")
-    data = pd.read_csv(args.filepath) 
-    return data
+def unzip(args):
+    folder_path = os.path.join("data", "fake_or_real_news.csv") # Defining the folder_path to the data.
+    if not os.path.exists(folder_path): # If the folder path does not exist, unzip the folder, if it exists do nothing 
+        print("Unzipping file")
+        path_to_zip = args.zip_path # Defining the path to the zip file
+        zip_destination = os.path.join("data") # defining the output destination
 
+        with zipfile.ZipFile(path_to_zip,"r") as zip_ref: # using the package from zipfile, to un zip the zip file
+            zip_ref.extractall(zip_destination) # Unzipping
+    print("The files are unzipped")
+    return folder_path
+
+
+def loading_data(folder_path):
+    print("Loading data")
+    data = pd.read_csv(folder_path) # Loading the data into a pandas dataframe
+    return data
 
 
 def pre_trained_model():
     print("Getting model")
-    classifier = pipeline("text-classification", 
-                      model="j-hartmann/emotion-english-distilroberta-base", 
+    classifier = pipeline("text-classification", # I am using huggingface's pipelin to load the text classifier
+                      model="j-hartmann/emotion-english-distilroberta-base", # The pretrained model
                       return_all_scores=False) # Only return the emotion with the highest score 
     return classifier
 
 
 def filtering(data):
-    print("Filtering on real or fake")
-    data_filtered_real = data[data['label'] == 'REAL']
-    data_filtered_fake = data[data['label'] == 'FAKE']
+    print("Creating REAL or FAKE dataframes")
+    data_filtered_real = data[data['label'] == 'REAL'] # Creating a dataframe of only REAL news
+    data_filtered_fake = data[data['label'] == 'FAKE'] # Creating a dataframe of only FAKE news
     return data_filtered_real, data_filtered_fake
 
 
 def data_cleaning(data, data_filtered_real, data_filtered_fake):
     print("Selecting column")
-    all_texts = data["title"] # Only selecting column "title" and storing in headlines
-    real_texts = data_filtered_real["title"]
-    fake_texts = data_filtered_fake["title"]
+    all_texts = data["title"] # Only selecting column "title"
+    real_texts = data_filtered_real["title"] # Only selecting column "title"
+    fake_texts = data_filtered_fake["title"] # Only selecting column "title"
     
     return all_texts, real_texts, fake_texts
 
@@ -54,12 +68,12 @@ def data_cleaning(data, data_filtered_real, data_filtered_fake):
 def emotion_loops(classifier, all_texts, real_texts, fake_texts):
     print("Getting emotion label for: ")
     emotion_list = [] # Creating an empty list to store each emotion label.
-    real_list = []
-    fake_list = []
+    real_list = [] # Empty list for real
+    fake_list = [] # Empty list for fake
     print("All...")
     for line in all_texts: # For loop that goes through each headline in the list headlines, and uses the classifier.
         emotion_list.append(classifier(line)[0]["label"])  # append list #output[0]["label"]
-        # using classifier to get emotion of each headline. The emotions are stored as dictionaries in a list. [0] gets the first dirctonary in the list ["label"] is they.
+        # using classifier to get emotion of each headline. The emotions are stored as Counter object dictionaries in a list. [0] gets the first dictionary in the list ["label"].
         # by only selecting label, I append it to the empty list emotion_list.
     print("Real...")   
     for line in real_texts: # The same logic in the code below
@@ -70,13 +84,17 @@ def emotion_loops(classifier, all_texts, real_texts, fake_texts):
     
     return emotion_list, real_list, fake_list
 
-def emotion_occurance(emotion_list, real_list, fake_list):
-    print("Preparing for visualization")
-    emotion_count = Counter(emotion_list) # Using counter from collections to get each occurance of the emotion 
-    real_emotion_count = Counter(real_list) 
-    fake_emotion_count = Counter(fake_list) 
 
-    return emotion_count, real_emotion_count, fake_emotion_count # the count is stored as a dictionary
+def count_emotions(emotion_list):
+    emotion_count = {}
+    for emotion in emotion_list:
+        if emotion in emotion_count:
+            emotion_count[emotion] += 1
+        else:
+            emotion_count[emotion] = 1
+    print(emotion_count)
+    return emotion_count
+
 
 def splitting_dictionary(emotion_count, real_emotion_count, fake_emotion_count):
     print("Preparing for visualization")
@@ -91,71 +109,69 @@ def splitting_dictionary(emotion_count, real_emotion_count, fake_emotion_count):
 
     return all_emotions, all_count, real_emotions, real_count, fake_emotions, fake_count
 
+def tabels(emotions, tablename):
+   # print("Creating and saving tabels")
+   # table = pd.DataFrame.from_dict(emotions, orient="index", columns = ["Count"]) # Creating a dataframe. Key is Index
     
-def visualization_bar(emotions, count, title, folder = "figs"):
-    print("Visualizing bar plots")
-    plt.bar(range(len(emotions)), count, tick_label=emotions)
-    plt.xlabel("Emotions")
-    plt.ylabel("Count")
-    plt.title(title)
-    plt.show()
+    #output_path = os.path.join("out", tablename + ".csv")
+    #table.to_csv(output_path, index= False) # Saving as csv
 
-    filename = os.path.join(folder, title.replace(' ', '_') + '.png')
+    print("Creating and saving tables")
+    table = pd.DataFrame(list(emotions.items()), columns=["Emotion", "Count"]) # Creating a DataFrame with the emotions and counts as columns
+    output_path = os.path.join("out", tablename + ".csv")
+    table.to_csv(output_path, index=False) # Save the DataFrame to a CSV file without including the index
+
+def visualization_bar(emotions, count, title, folder = "figs"): # Creating a bar graph to visualise the emotions. Can be used for all, real, or fake
+    print("Visualizing bar plots")
+    plt.bar(emotions, count)  # X axis is emotions, Y axis is count 
+    plt.xlabel("Emotions") # X axis label
+    plt.ylabel("Count") # Y axis label
+    plt.title(title) # Giving it a title
+
+    filename = os.path.join(folder, title.replace(' ', '_') + '.png') # Saving the plot. Remove space with underscore in the title
     plt.savefig(filename)
-    plt.clf()
+    plt.clf() # Clearing the plot
 
 def visualization_scatter(all_emotions, all_count, real_emotions, real_count, fake_emotions, fake_count, folder = "figs"):
     print("Visualizing scatterplot")
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots() 
+    # Plotting all, real, and fake in one scatter plot
+    ax.scatter(all_emotions, all_count, c='blue', label='All Emotions', marker= "o") # Creating all emotion points as a blue circle
 
-    ax.scatter(all_emotions, all_count, c='blue', label='All Emotions', marker= "o")
-    #ax.plot(all_emotions, all_count, c='blue', label='All Emotions')
+    ax.scatter(real_emotions, real_count, c='green', label='Real Emotions', marker= "+" ) # Creating real emotions as a green plus sign
 
-    ax.scatter(real_emotions, real_count, c='green', label='Real Emotions', marker= "+" )
-    #ax.plot(real_emotions, real_count, c='green', label='Real Emotions')
+    ax.scatter(fake_emotions, fake_count, c='red', label='Fake Emotions', marker= "_") # Creating fake emotions as a red underscore
 
-    ax.scatter(fake_emotions, fake_count, c='red', label='Fake Emotions', marker= "_")
-    #ax.plot(fake_emotions, fake_count, c='red', label='Fake Emotions')
+    ax.set_xlabel('Emotion') # X axis label
+    ax.set_ylabel('Count') # Y axis label
+    plt.legend() # Creating a legend
+    plt.title('Emotion Counts') # Title
 
-    ax.set_xlabel('Emotion')
-    ax.set_ylabel('Count')
-    plt.legend()
-    plt.title('Emotion Counts')
-
-    filename = os.path.join(folder, "scatterplot" + '.png')
+    filename = os.path.join(folder, "scatterplot" + '.png') # Saving the plot
     plt.savefig(filename)
-    plt.clf()
+    plt.clf() # Clearing plot
 
 def main_function():
-    args = input_parse()
-    data = loading_data(args)
-    classifier = pre_trained_model()
-    data_filtered_real, data_filtered_fake = filtering(data)
-    all_texts, real_texts, fake_texts = data_cleaning(data, data_filtered_real, data_filtered_fake)
-    emotion_list, real_list, fake_list = emotion_loops(classifier, all_texts, real_texts, fake_texts)
-    emotion_count, real_emotion_count, fake_emotion_count = emotion_occurance(emotion_list, real_list, fake_list)
-    all_emotions, all_count, real_emotions, real_count, fake_emotions, fake_count = splitting_dictionary(emotion_count, real_emotion_count, fake_emotion_count)
-    visualization_bar(all_emotions, all_count, "All Headings", folder = "figs")
-    visualization_bar(real_emotions, real_count, "Real Headings", folder = "figs")
-    visualization_bar(fake_emotions, fake_count, "Fake Headings", folder = "figs")
-    visualization_scatter(all_emotions, all_count, real_emotions, real_count, fake_emotions, fake_count, folder = "figs")
+    args = input_parse() # Command line arguments 
+    folder_path = unzip(args) # Unzipping zip folder
+    data = loading_data(folder_path) # Loading data into pandas dataframe
+    classifier = pre_trained_model() # Getting pretrained model
+    data_filtered_real, data_filtered_fake = filtering(data) # Creating two new dataframes with fake or real
+    all_texts, real_texts, fake_texts = data_cleaning(data, data_filtered_real, data_filtered_fake) # Selecting column
+    emotion_list, real_list, fake_list = emotion_loops(classifier, all_texts, real_texts, fake_texts) # Getting the emotion for each headline
+   
+    emotion_count = count_emotions(emotion_list)
+    real_emotion_count = count_emotions(real_list)
+    fake_emotion_count = count_emotions(fake_list)
 
-if __name__ == "__main__":
+    all_emotions, all_count, real_emotions, real_count, fake_emotions, fake_count = splitting_dictionary(emotion_count, real_emotion_count, fake_emotion_count) # Splitting the values
+    tabels(emotion_count, "all_emotions") # Creating and saving table for all emotions
+    tabels(real_emotion_count, "real_emotions") # Creating and saving table for real emotions
+    tabels(fake_emotion_count, "fake_emotions") # Creating and saving table for fake emotions
+    visualization_bar(all_emotions, all_count, "All Headings", folder = "figs") # Creating bar plot for all emotions
+    visualization_bar(real_emotions, real_count, "Real Headings", folder = "figs") # Creating bar plot for real emotions
+    visualization_bar(fake_emotions, fake_count, "Fake Headings", folder = "figs") # Creating bar plot for fake emotions
+    visualization_scatter(all_emotions, all_count, real_emotions, real_count, fake_emotions, fake_count, folder = "figs") # Creating a scatter plot
+
+if __name__ == "__main__": # If script is called from command line fun main function
     main_function()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
